8d8497f0ff6f027497ccda271268c32473e582e0
yarion1 <yariongranham@gmail.com>
2024-02-16 18:56:48 -0300

    [feature-0002]-add cors and class validators, enable validation pipe and create global prefix for api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'log', 'warn'],
+  });
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
